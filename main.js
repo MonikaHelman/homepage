b27ee788269f21124646f0c563144b918f59bc19
Monika Helman <monika.w007@gmail.com>
2020-05-17 02:17:00 +0200

    innerHTML added

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 alert('Czesc, dobrze ze jestes!');
+
+const firstName = 'Monika'
+const secondName = 'Helman'
+
+console.log (
+    `Nazywam sie ${firstName} ${secondName}`)
+
+ const footer = document.querySelector('.footer--js')    
+ console.log(footer);
+ footer.innerHTML = `Nazywam sie ${firstName} ${secondName}`
+ console.log(footer);
